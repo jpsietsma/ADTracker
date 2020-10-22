5810cfaf85e21609169e9a7a8dc73f431641f378
jpsietsma <jpsietsma@gmail.com>
2020-10-22 19:40:39 -0400

    Add project files.

---
+++ ADT.Models/TransportationVehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ADT.Models
+{
+    public class TransportationVehicle
+    {
+        public int VehicleId { get; set; }
+        public string VehicleMake { get; set; }
+        public string VehicleModel { get; set; }
+        public int VehicleYear { get; set; }
+
+        public TransportationVehicle()
+        {
+
+        }
+    }
+}
